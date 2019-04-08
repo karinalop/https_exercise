20da5090b4de7df3a58437cb423f6f3ef3be5c36
karinalop <karina.popeye@gmail.com>
2019-04-08 21:21:57 +0000

    added print_uppercase.js

---
+++ callbacks/print_uppercase.js
@@ -0,0 +1,16 @@
+var getHTML = require('../http-functions');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+var print_uppercase = function(data){
+  console.log(data);
+  data = data.toUpperCase();
+  console.log(data);
+
+}
+
+
+getHTML (requestOptions, print_uppercase);
