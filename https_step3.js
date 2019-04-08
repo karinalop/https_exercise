5b5c9b9db53fd4b9cccb5ef5cafc9470e9f57d00
karinalop <karina.popeye@gmail.com>
2019-04-08 19:30:13 +0000

    Added step 3

--- https_step3.js
+++ https_step3.js
@@ -1,1 +1,38 @@
-https_step3.js
+
+var https = require('https');
+
+function getAndPrintHTML (options) {
+
+  /* Add your code here */
+  https.get(options, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  var dataBuffer = "";
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    //console.log('Chunk Received. Length:', data.length);
+    //console.log(data + '\n');
+    dataBuffer += data;
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+
+  response.on('end', function() {
+    //console.log("endo of the stream");
+    console.log(dataBuffer);
+  });
+
+});
+
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML (requestOptions);
