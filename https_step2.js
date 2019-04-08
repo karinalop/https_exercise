d85c5e176ecc8971833d7faf67c613df7129c736
karinalop <karina.popeye@gmail.com>
2019-04-08 19:25:51 +0000

    Added steps 1,2

---
+++ https_step2.js
@@ -0,0 +1,38 @@
+
+// while https is built-in to Node, it is a module, so it must be required
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+   https.get(requestOptions, function (response) {
+
+  // set encoding of received data to UTF-8
+  response.setEncoding('utf8');
+
+  var dataBuffer = "";
+
+  // the callback is invoked when a `data` chunk is received
+  response.on('data', function (data) {
+    //console.log('Chunk Received. Length:', data.length);
+    //console.log(data + '\n');
+    dataBuffer += data;
+  });
+
+  // the callback is invoked when all of the data has been received
+  // (the `end` of the stream)
+
+  response.on('end', function() {
+    //console.log("endo of the stream");
+    console.log(dataBuffer);
+  });
+
+});
+
+}
+
+getAndPrintHTML ();
---
+++ https_step3.js
@@ -0,0 +1,1 @@
+https_step3.js
